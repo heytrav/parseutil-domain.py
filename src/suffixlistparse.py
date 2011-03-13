6cd03da575277903f14450042ac5a2bc0662afcb
William Travis Holton <wtholton@googlemail.com>
2011-03-13 22:24:19 +0100

    regex for parsing the suffix file

--- src/suffixlistparse.py
+++ src/suffixlistparse.py
@@ -14,8 +14,16 @@ suffixfilename = ''
 for option, value in options[0]:
     if option == '-i': suffixfilename = value
 
+compiledRegex = re.compile('''
+  
+  \A
+    ( 
+       [^\s\/].*
+    )
 
+  \z
+
+''', re.VERBOSE)
 
 print suffixfilename
 content = open(suffixfilename).read()
-print content
